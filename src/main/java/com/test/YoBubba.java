90ceb87b0a63f3a3abf262309bf28927d666b0e5
Ralph Saunders <ralph@keystone.com>
2012-05-24 14:57:06 -0400

    Initial revision.

---
+++ src/main/java/com/test/YoBubba.java
@@ -0,0 +1,21 @@
+package com.test;
+
+/**
+ * Created by
+ * User: ralph
+ * Date: 12/6/11
+ * Time: 2:20 PM
+ */
+public class YoBubba {
+
+    public  String  getMessage() {
+
+        return "Yo Bubba!";
+    }
+
+    public static void main(String[] args) {
+        YoBubba     yb = new YoBubba();
+
+        System.out.println(yb.getMessage());
+    }
+}
